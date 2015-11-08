206c95b82dd12d0352566128c1977bfaf56d65f9
Cassieyang <cassie_yang2012@163.com>
2015-11-08 23:03:37 +0800

    Add gridview
    Add gridview and creat adapter

--- app/src/main/java/com/example/cassie/projectone/MainActivityFragment.java
+++ app/src/main/java/com/example/cassie/projectone/MainActivityFragment.java
@@ -5,19 +5,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
+    private GridView Gridview_Movies1;
+    private int[] ids;
     public MainActivityFragment() {
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        Gridview_Movies1 = (GridView) rootView. findViewById(R.id.Gridview_Movies);
+        ArrayAdapter<String> mMoviesAdapter= new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.grid_fragment,
+                R.id.imageView_Movies,
+                ids[]);
+        GridView listView = (GridView) rootView.findViewById(
+                R.id.Gridview_Movies);
+        listView.setAdapter(mMoviesAdapter);
+        return rootView;
     }
 }
